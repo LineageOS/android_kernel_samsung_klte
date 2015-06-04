5185c04f4439dd99f69d8b2206a8529801564112
Zhen Kong <zkong@codeaurora.org>
2015-06-03 21:50:40 -0700

    crypto: msm: remove wakeup lock in qcrypto driver
    When log server still runs and try to enable CE clock just before sleep,
    wakeup lock will prevent the device sleep. And the bam/crypto is a not a
    wakeupable device. As we have a state machine in crypto driver, and it
    is able to control suspend behavior well, we do not need to use wakeup
    lock in qcrypto driver.
    
    Change-Id: I660818ec6fad4051db08c10a76d5d1b408bece7e
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>

--- drivers/crypto/msm/qcrypto.c
+++ drivers/crypto/msm/qcrypto.c
@@ -455,7 +455,6 @@ static void qcrypto_ce_set_bus(struct crypto_engine *pengine,
 	int ret = 0;
 
 	if (high_bw_req) {
-		pm_stay_awake(&pengine->pdev->dev);
 		ret = qce_enable_clk(pengine->qce);
 		if (ret) {
 			pr_err("%s Unable enable clk\n", __func__);
@@ -469,10 +468,7 @@ static void qcrypto_ce_set_bus(struct crypto_engine *pengine,
 			qce_disable_clk(pengine->qce);
 			goto clk_err;
 		}
-
-
 	} else {
-
 		ret = msm_bus_scale_client_update_request(
 				pengine->bus_scale_handle, 0);
 		if (ret) {
@@ -490,11 +486,8 @@ static void qcrypto_ce_set_bus(struct crypto_engine *pengine,
 						__func__);
 			goto clk_err;
 		}
-		pm_relax(&pengine->pdev->dev);
 	}
-	return;
 clk_err:
-	pm_relax(&pengine->pdev->dev);
 	return;
 
 }
