9d021f12ebca699672891f8ee87e43a5db4aca98
VijayaKumar T M <vtmuni@codeaurora.org>
2016-11-14 11:15:43 +0530

    msm: camera: Restructure data handling to be more robust
    Use dynamic array allocation instead of static array to
    prevent stack overflow.
    User-supplied number of bytes may result in integer overflow.
    To fix this we check that the num_byte isn't above 8K size.
    
    CRs-Fixed: 1060554
    Change-Id: I9b05b846e5cc3a62b1a0a67be529f09abc764796
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_qup_i2c.c
    	include/media/msm_cam_sensor.h

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_qup_i2c.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_qup_i2c.c
@@ -140,13 +140,11 @@ int32_t msm_camera_qup_i2c_read_seq(struct msm_camera_i2c_client *client,
 		pr_err("%s: integer overflow prevented\n", __func__);
 		return rc;
 	}
-
 	buf = kzalloc(client->addr_type+num_byte, GFP_KERNEL);
 	if (!buf) {
 		pr_err("%s:%d no memory\n", __func__, __LINE__);
 		return -ENOMEM;
 	}
-
 	if (client->addr_type == MSM_CAMERA_I2C_BYTE_ADDR) {
 		buf[0] = addr;
 	} else if (client->addr_type == MSM_CAMERA_I2C_WORD_ADDR) {
