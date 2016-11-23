7b53989ff3490e00deb02e907bbb11b66056662f
VijayaKumar T M <vtmuni@codeaurora.org>
2016-11-23 11:43:56 +0530

    msm: camera: restructure data handling to be more robust
    Add dynamic array allocation instead of static to prevent
    stack overflow.
    
    CRs-Fixed: 789704
    Change-Id: Id12ed5b01809021d2b1d1d71436f2523b575d9de
    Signed-off-by: Vasko Kalanoski <vaskok@codeaurora.org>
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c
@@ -176,6 +176,11 @@ int32_t msm_camera_cci_i2c_write_seq(struct msm_camera_i2c_client *client,
 	     && client->addr_type != MSM_CAMERA_I2C_WORD_ADDR)
 	    || num_byte == 0)
 		return rc;
+	if (num_byte > I2C_SEQ_REG_DATA_MAX) {
+		pr_err("%s: num_byte=%d clamped to max supported %d\n",
+			__func__, num_byte, I2C_SEQ_REG_DATA_MAX);
+		return rc;
+	}
 
 	S_I2C_DBG("%s reg addr = 0x%x num bytes: %d\n",
 		__func__, addr, num_byte);
@@ -256,6 +261,7 @@ int32_t msm_camera_cci_i2c_write_burst(struct msm_camera_i2c_client *client,
 
 	rc = cci_ctrl.status;
 	kfree(reg_conf_tbl);
+	reg_conf_tbl = NULL;
 	return rc;
 }
 
