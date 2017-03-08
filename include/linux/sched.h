dbc16c5322895a5a17de0d241d4aaacc319ca5b3
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-03-08 19:55:56 +0100

    sched: fix duplicated include
    Change-Id: Ibbde1e00c830b58355291e07e635095d7044e8c9

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1371,8 +1371,6 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
-	unsigned long atomic_flags; /* Flags needing atomic access. */
-
 	pid_t pid;
 	pid_t tgid;
 
